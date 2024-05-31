77d4ac61dc2e5489e722019aa07ba146bad50202
StyleShit <32631382+StyleShit@users.noreply.github.com>
2024-05-31 13:29:27 +0300

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import globals from 'globals';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
+	eslint.configs.recommended,
+	...tseslint.configs.strictTypeChecked,
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node,
+			},
+			parserOptions: {
+				project: true,
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
+	},
+	{
+		ignores: [
+			'**/coverage/**',
+			'**/dist/**',
+			'**/node_modules/**',
+			'**/__snapshots__/**',
+		],
+	},
+);
