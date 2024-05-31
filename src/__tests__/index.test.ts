b4a7ac5246f5237a6a4c57552d8b7e2dafcb2da5
StyleShit <evyatar.daud@gmail.com>
2024-05-31 17:19:02 +0300

    chore: remove unused test

--- src/__tests__/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from 'vitest';
-import helloWorld from '../index';
-
-describe('Hello World', () => {
-	it('should return "Hello World!"', () => {
-		expect(helloWorld()).toBe('Hello World!');
-	});
-});
